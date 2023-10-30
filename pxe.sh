41b872bafcca39a7aa4af43417e7d68a68e4d354
Dristen <dristenmcknight@gmail.com>
2023-10-30 15:44:04 -0400

    fix bug where boot option was 000<letter>

--- pxe.sh
+++ pxe.sh
@@ -15,7 +15,7 @@ if echo "$first_boot_option" | grep -q "PXE IPv4"; then
     echo "PXE boot is already the first option in the boot order"
 else
     # Get PXE boot option
-    pxe_boot="$(efibootmgr -v | grep -E "^Boot[0-9]{4}" | grep "PXE IPv4" | cut -c5-8)"
+    pxe_boot="$(efibootmgr -v | grep -E "^Boot[0-9,A-Z]{4}" | grep "PXE IPv4" | cut -c5-8)"
     
     # Set the boot order to PXE boot followed by the other options
     new_boot_order="$(echo "$pxe_boot"),$(echo "$os_boot")"
